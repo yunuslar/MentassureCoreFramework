a9f65353aa2631208308feb721380ba8f57f75ac
yunuslar <aliyunuslar@gmail.com>
2018-03-12 00:08:32 +0300

    MentassureCore Framework V1.0
    MentassureCore Framework V1.0

---
+++ MentassureCore.framework/Headers/MentassureCore.h
@@ -0,0 +1,19 @@
+//
+//  MentassureCore.h
+//  MentassureCore
+//
+//  Created by yunuslar on 6.03.2018.
+//  Copyright © 2018 MentaVision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MentassureCore.
+FOUNDATION_EXPORT double MentassureCoreVersionNumber;
+
+//! Project version string for MentassureCore.
+FOUNDATION_EXPORT const unsigned char MentassureCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MentassureCore/PublicHeader.h>
+
+
